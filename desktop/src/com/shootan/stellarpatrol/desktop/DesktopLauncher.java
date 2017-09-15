eca599a3d5cf95e3dc5c04dbf178c9951e1f3501
geeksploit <geeksploit@shootan.com>
2017-09-15 11:42:06 +0300

    Create a fresh project with LibGDX Project Generator.

---
+++ desktop/src/com/shootan/stellarpatrol/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.shootan.stellarpatrol.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.shootan.stellarpatrol.StellarPatrolGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StellarPatrolGame(), config);
+	}
+}
---
+++ html/src/com/shootan/stellarpatrol/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.shootan.stellarpatrol.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.shootan.stellarpatrol.StellarPatrolGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new StellarPatrolGame();
+        }
+}
