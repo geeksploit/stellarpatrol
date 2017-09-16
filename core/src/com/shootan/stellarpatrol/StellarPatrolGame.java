552acf39473edba3054523b923414caa5ab5b33c
geeksploit <geeksploit@shootan.com>
2017-09-16 12:02:40 +0300

    Implement showDifficultyScreen() method.

--- core/src/com/shootan/stellarpatrol/StellarPatrolGame.java
+++ core/src/com/shootan/stellarpatrol/StellarPatrolGame.java
@@ -1,10 +1,15 @@
 package com.shootan.stellarpatrol;
 
 import com.badlogic.gdx.Game;
+import com.shootan.stellarpatrol.screens.DifficultyScreen;
 
 public class StellarPatrolGame extends Game {
 
 	@Override
 	public void create() {
+	}
+
+	public void showDifficultyScreen() {
+		setScreen(new DifficultyScreen(this));
 	}
 }
