7470e8570912f22f9cea137c49fac29c288bf86a
geeksploit <geeksploit@shootan.com>
2017-09-16 12:01:26 +0300

    Create DifficultyScreen class stub.

---
+++ core/src/com/shootan/stellarpatrol/screens/DifficultyScreen.java
@@ -0,0 +1,49 @@
+package com.shootan.stellarpatrol.screens;
+
+import com.badlogic.gdx.Screen;
+import com.shootan.stellarpatrol.StellarPatrolGame;
+
+/**
+ * Created by geeksploit on 16.09.2017.
+ */
+
+public class DifficultyScreen implements Screen {
+
+    public DifficultyScreen(StellarPatrolGame game) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
