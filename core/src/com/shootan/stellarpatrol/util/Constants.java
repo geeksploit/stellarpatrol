03c3ec9dbdf5b22be463c77f4683f2cd92654d25
geeksploit <geeksploit@shootan.com>
2017-09-16 13:20:45 +0300

    Define a few constants to store difficulty colors.

--- core/src/com/shootan/stellarpatrol/util/Constants.java
+++ core/src/com/shootan/stellarpatrol/util/Constants.java
@@ -1,5 +1,7 @@
 package com.shootan.stellarpatrol.util;
 
+import com.badlogic.gdx.graphics.Color;
+
 /**
  * Created by geeksploit on 16.09.2017.
  */
@@ -8,4 +10,7 @@ public class Constants {
     public static final float DIFFICULTY_WORLD_SIZE = 480.0f;
     public static final float DIFFICULTY_BUBBLE_RADIUS = DIFFICULTY_WORLD_SIZE / 9;
     public static final float DIFFICULTY_LABEL_SCALE = 1.5f;
+    public static final Color EASY_COLOR = new Color(0.2f, 0.5f, 1, 0.5f);
+    public static final Color MEDIUM_COLOR = new Color(0.5f, 0.75f, 1, 0.5f);
+    public static final Color HARD_COLOR = new Color(0.7f, 1f, 1, 0.5f);
 }
