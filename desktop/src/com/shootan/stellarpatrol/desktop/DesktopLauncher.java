f0f2d6e16569c6b2cb43db2fa00544d83f2c85db
geeksploit <geeksploit@shootan.com>
2017-09-16 18:01:56 +0300

    Set desktop application name and icons.

--- desktop/src/com/shootan/stellarpatrol/desktop/DesktopLauncher.java
+++ desktop/src/com/shootan/stellarpatrol/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.shootan.stellarpatrol.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.shootan.stellarpatrol.StellarPatrolGame;
@@ -7,6 +8,10 @@ import com.shootan.stellarpatrol.StellarPatrolGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.addIcon("icons/icon_128x128.png", Files.FileType.Internal);
+		config.addIcon("icons/icon_32x32.png", Files.FileType.Internal);
+		config.addIcon("icons/icon_16x16.png", Files.FileType.Internal);
+		config.title = "Stellar Patrol";
 		new LwjglApplication(new StellarPatrolGame(), config);
 	}
 }
