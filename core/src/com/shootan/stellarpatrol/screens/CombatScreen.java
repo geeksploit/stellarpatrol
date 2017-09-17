34f3f7912c09689f54b496cc9ea9e5b53d20c3bb
geeksploit <geeksploit@shootan.com>
2017-09-17 12:14:13 +0300

    Declare and initialize the combat viewport.

--- core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
+++ core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
@@ -1,7 +1,9 @@
 package com.shootan.stellarpatrol.screens;
 
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.shootan.stellarpatrol.StellarPatrolGame;
+import com.shootan.stellarpatrol.util.Constants;
 
 /**
  * Created by geeksploit on 17.09.2017.
@@ -11,13 +13,15 @@ public class CombatScreen implements Screen {
 
     private StellarPatrolGame game;
 
+    private ExtendViewport combatViewport;
+
     public CombatScreen(StellarPatrolGame game) {
         this.game = game;
     }
 
     @Override
     public void show() {
-
+        combatViewport = new ExtendViewport(Constants.WORLD_SIZE, Constants.WORLD_SIZE);
     }
 
     @Override
