aa171d03ed7062bf67cbb45936efb8e723ac0f1e
geeksploit <geeksploit@shootan.com>
2017-09-17 22:33:07 +0300

    Define abstract weapon method stubs.

--- core/src/com/shootan/stellarpatrol/equipment/weapons/AbstractWeapon.java
+++ core/src/com/shootan/stellarpatrol/equipment/weapons/AbstractWeapon.java
@@ -1,5 +1,10 @@
 package com.shootan.stellarpatrol.equipment.weapons;
 
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.shootan.stellarpatrol.gameobjects.GameObject;
+
 /**
  * Created by geeksploit on 17.09.2017.
  */
@@ -7,5 +12,30 @@ package com.shootan.stellarpatrol.equipment.weapons;
 abstract class AbstractWeapon implements Weapon {
 
     AbstractWeapon() {
+    }
+
+    @Override
+    public Array<GameObject> shoot(float deltaTime, Vector2 parentPosition, float parentAngle) {
+        return null;
+    }
+
+    @Override
+    public void setReloadCooldown(float reloadCooldown) {
+
+    }
+
+    @Override
+    public float getReloadCooldown() {
+        return 0;
+    }
+
+    @Override
+    public void setBulletClass(Class bulletClass) {
+
+    }
+
+    @Override
+    public Matrix3 getOriginTransformMatrix() {
+        return null;
     }
 }
