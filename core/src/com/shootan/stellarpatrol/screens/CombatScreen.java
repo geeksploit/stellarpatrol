0c8aa2ae43c8fb130a16a5f6566c6fc78f798478
geeksploit <geeksploit@shootan.com>
2017-09-17 12:08:22 +0300

    Implement Screen interface.

--- core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
+++ core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
@@ -1,16 +1,52 @@
 package com.shootan.stellarpatrol.screens;
 
+import com.badlogic.gdx.Screen;
 import com.shootan.stellarpatrol.StellarPatrolGame;
 
 /**
  * Created by geeksploit on 17.09.2017.
  */
 
-public class CombatScreen {
+public class CombatScreen implements Screen {
 
     private StellarPatrolGame game;
 
     public CombatScreen(StellarPatrolGame game) {
         this.game = game;
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
     }
 }
