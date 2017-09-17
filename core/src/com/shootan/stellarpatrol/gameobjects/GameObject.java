6d0550cad65b9b71090c6185450fc99e2309a579
geeksploit <geeksploit@shootan.com>
2017-09-17 18:49:46 +0300

    Implement constructor that accepts position as a parameter.

--- core/src/com/shootan/stellarpatrol/gameobjects/GameObject.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/GameObject.java
@@ -1,8 +1,13 @@
 package com.shootan.stellarpatrol.gameobjects;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by geeksploit on 17.09.2017.
  */
 
 public abstract class GameObject {
+
+    public GameObject(Vector2 position) {
+    }
 }
