22d071f43e3ac425279201cfbd0a4c14c9c15ea3
geeksploit <geeksploit@shootan.com>
2017-09-17 12:06:18 +0300

    Implement combat screen constructor.

--- core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
+++ core/src/com/shootan/stellarpatrol/screens/CombatScreen.java
@@ -1,8 +1,13 @@
 package com.shootan.stellarpatrol.screens;
 
+import com.shootan.stellarpatrol.StellarPatrolGame;
+
 /**
  * Created by geeksploit on 17.09.2017.
  */
 
 public class CombatScreen {
+
+    public CombatScreen(StellarPatrolGame game) {
+    }
 }
