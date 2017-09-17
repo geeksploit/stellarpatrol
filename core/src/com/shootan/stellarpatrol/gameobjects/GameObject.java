5dd5ddcd77d75ea954d1c52f18862316c7e775e1
geeksploit <geeksploit@shootan.com>
2017-09-17 18:51:01 +0300

    Implement position getter and setter.

--- core/src/com/shootan/stellarpatrol/gameobjects/GameObject.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/GameObject.java
@@ -13,4 +13,12 @@ public abstract class GameObject {
     public GameObject(Vector2 position) {
         this.position = position;
     }
+
+    public Vector2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector2 position) {
+        this.position = position;
+    }
 }
