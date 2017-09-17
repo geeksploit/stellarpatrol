eb0591a1f70ff38d66218cf5f4476dab376af178
geeksploit <geeksploit@shootan.com>
2017-09-17 21:02:36 +0300

    Declare shoot method.

--- core/src/com/shootan/stellarpatrol/equipment/weapons/Weapon.java
+++ core/src/com/shootan/stellarpatrol/equipment/weapons/Weapon.java
@@ -1,8 +1,14 @@
 package com.shootan.stellarpatrol.equipment.weapons;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
+import com.shootan.stellarpatrol.gameobjects.GameObject;
+
 /**
  * Created by geeksploit on 17.09.2017.
  */
 
 public interface Weapon {
+
+    Array<GameObject> shoot(float deltaTime, Vector2 parentPosition, float parentAngle);
 }
