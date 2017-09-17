f7d3e4ee17c755137a214bf9e7d0954d0687d75e
geeksploit <geeksploit@shootan.com>
2017-09-17 15:21:48 +0300

    Assume one of the difficulty bubbles was hit.

--- core/src/com/shootan/stellarpatrol/screens/DifficultyScreen.java
+++ core/src/com/shootan/stellarpatrol/screens/DifficultyScreen.java
@@ -132,6 +132,7 @@ public class DifficultyScreen extends InputAdapter implements Screen {
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         Vector2 touch = extendViewport.unproject(new Vector2(screenX, screenY));
-        return super.touchUp(screenX, screenY, pointer, button);
+        boolean hit = true;
+        return hit;
     }
 }
