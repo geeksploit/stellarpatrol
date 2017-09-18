f013d72bdef279c9c49236feeaf91f3a895c7f35
geeksploit <geeksploit@shootan.com>
2017-09-18 17:02:31 +0300

    Declare and initialize a delayed removal array to store game objects

--- core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
@@ -1,11 +1,16 @@
 package com.shootan.stellarpatrol.gameobjects;
 
+import com.badlogic.gdx.utils.DelayedRemovalArray;
+
 /**
  * Created by geeksploit on 18.09.2017.
  */
 
 public final class GameObjectsContainer {
 
+    private DelayedRemovalArray<GameObject> gameObjects;
+
     public GameObjectsContainer() {
+        gameObjects = new DelayedRemovalArray<GameObject>();
     }
 }
