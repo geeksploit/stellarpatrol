d834a0e0fb38a7ccba5cac583605c9b6407d3c46
geeksploit <geeksploit@shootan.com>
2017-09-18 13:53:21 +0300

    Declare and initialize a field to store bullets

--- core/src/com/shootan/stellarpatrol/gameobjects/ships/Ship.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Ship.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.shootan.stellarpatrol.equipment.weapons.Weapon;
 import com.shootan.stellarpatrol.gameobjects.GameObject;
+import com.shootan.stellarpatrol.gameobjects.bullets.Bullet;
 
 /**
  * Created by geeksploit on 18.09.2017.
@@ -13,10 +14,12 @@ public abstract class Ship extends GameObject {
 
     private float hitPoints;
     private Array<Weapon> weapons;
+    private Array<Bullet> bullets;
 
     public Ship(Vector2 position) {
         super(position);
         weapons = new Array<Weapon>();
+        bullets = new Array<Bullet>();
     }
 
     public float getHitPoints() {
