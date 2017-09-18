676a8c132b656c0a46fe70cea99245aa10683e8a
geeksploit <geeksploit@shootan.com>
2017-09-18 15:15:13 +0300

    Fix bullet factory to accept a weapon as a parameter

--- core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletFactory.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletFactory.java
@@ -2,6 +2,7 @@ package com.shootan.stellarpatrol.gameobjects.bullets;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.shootan.stellarpatrol.equipment.weapons.Weapon;
 
 /**
  * Created by geeksploit on 18.09.2017.
@@ -23,7 +24,7 @@ public class BulletFactory {
         return instance;
     }
 
-    public Array<Bullet> makeBullets(Class bulletClass, Vector2 position, float angle) {
+    public Array<Bullet> makeBullets(Weapon weapon, Vector2 position, float angle) {
         bullets.clear();
         return bullets;
     }
