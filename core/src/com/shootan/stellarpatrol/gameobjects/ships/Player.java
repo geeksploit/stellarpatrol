5118cb4ef1e4634b1f59092388a8955af02a5956
geeksploit <geeksploit@shootan.com>
2017-09-18 15:32:30 +0300

    Set player ship texture

--- core/src/com/shootan/stellarpatrol/gameobjects/ships/Player.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Player.java
@@ -1,6 +1,7 @@
 package com.shootan.stellarpatrol.gameobjects.ships;
 
 import com.badlogic.gdx.math.Vector2;
+import com.shootan.stellarpatrol.util.Constants;
 
 /**
  * Created by geeksploit on 18.09.2017.
@@ -10,5 +11,6 @@ public final class Player extends Ship {
 
     public Player(Vector2 position) {
         super(position);
+        setTextureRegion(Constants.SHIP_TEXTURE_REGIONS.get(0));
     }
 }
