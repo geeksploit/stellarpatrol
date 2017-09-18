f4238c352b635f8772267233da3e4fc0a6365975
geeksploit <geeksploit@shootan.com>
2017-09-18 05:14:03 +0300

    Declare and initialize bullets array.

--- core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletFactory.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletFactory.java
@@ -1,5 +1,8 @@
 package com.shootan.stellarpatrol.gameobjects.bullets;
 
+import com.badlogic.gdx.utils.Array;
+import com.shootan.stellarpatrol.gameobjects.GameObject;
+
 /**
  * Created by geeksploit on 18.09.2017.
  */
@@ -7,8 +10,10 @@ package com.shootan.stellarpatrol.gameobjects.bullets;
 public class BulletFactory {
 
     private static BulletFactory instance;
+    private Array<GameObject> bullets;
 
     private BulletFactory() {
+        bullets = new Array<GameObject>();
     }
 
     public BulletFactory getInstance() {
