403b6301053d988ede9002bfeb1a329754d8de21
geeksploit <geeksploit@shootan.com>
2017-09-18 15:00:21 +0300

    Set primary bullet texture

--- core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletPrimary.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletPrimary.java
@@ -1,6 +1,7 @@
 package com.shootan.stellarpatrol.gameobjects.bullets;
 
 import com.badlogic.gdx.math.Vector2;
+import com.shootan.stellarpatrol.util.Constants;
 
 /**
  * Created by geeksploit on 18.09.2017.
@@ -10,5 +11,6 @@ public final class BulletPrimary extends Bullet {
 
     public BulletPrimary(Vector2 position, float angle) {
         super(position, angle);
+        setTextureRegion(Constants.BULLET_TEXTURE_REGIONS.get(1));
     }
 }
