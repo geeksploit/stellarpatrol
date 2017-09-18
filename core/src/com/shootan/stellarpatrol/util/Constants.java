d2b4c4cce8465ffc9dfa39a6835480629ce84add
geeksploit <geeksploit@shootan.com>
2017-09-18 16:13:51 +0300

    Define a constant for player weapon default cooldown

--- core/src/com/shootan/stellarpatrol/util/Constants.java
+++ core/src/com/shootan/stellarpatrol/util/Constants.java
@@ -23,6 +23,7 @@ public class Constants {
     public static final float WORLD_SIZE = 10;
 
     public static final float WEAPON_DEFAULT_COOLDOWN = 5;
+    public static final float PLAYER_WEAPON_COOLDOWN = WEAPON_DEFAULT_COOLDOWN / 10;
     public static final float BULLET_MAX_VELOCITY = 10f;
 
     private static final float HARD_SPAWN_COOLDOWN = .075f;
