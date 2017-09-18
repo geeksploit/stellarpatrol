39dc1b23ad44a7d18314f596118f7ce14f821c0d
geeksploit <geeksploit@shootan.com>
2017-09-18 13:38:49 +0300

    Add abstract ship class stub to version control.

---
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Ship.java
@@ -0,0 +1,15 @@
+package com.shootan.stellarpatrol.gameobjects.ships;
+
+import com.badlogic.gdx.math.Vector2;
+import com.shootan.stellarpatrol.gameobjects.GameObject;
+
+/**
+ * Created by geeksploit on 18.09.2017.
+ */
+
+public abstract class Ship extends GameObject {
+
+    public Ship(Vector2 position) {
+        super(position);
+    }
+}
