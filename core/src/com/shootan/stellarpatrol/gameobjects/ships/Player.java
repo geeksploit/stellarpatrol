4cb5c3e9fade5364fbfb4c678fcd0ab0292b5ae4
geeksploit <geeksploit@shootan.com>
2017-09-18 16:16:32 +0300

    Give player a weapon

--- core/src/com/shootan/stellarpatrol/gameobjects/ships/Player.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Player.java
@@ -1,6 +1,7 @@
 package com.shootan.stellarpatrol.gameobjects.ships;
 
 import com.badlogic.gdx.math.Vector2;
+import com.shootan.stellarpatrol.equipment.weapons.PrimaryWeapon;
 import com.shootan.stellarpatrol.util.Constants;
 
 /**
@@ -13,5 +14,6 @@ public final class Player extends Ship {
         super(position);
         setTextureRegion(Constants.SHIP_TEXTURE_REGIONS.get(0));
         setScale(1);
+        addWeapons(new PrimaryWeapon(Constants.PLAYER_WEAPON_COOLDOWN));
     }
 }
