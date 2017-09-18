4420f5f27aa910f55eddcaa307c8ce0ae2c2db0b
geeksploit <geeksploit@shootan.com>
2017-09-18 14:47:31 +0300

    Add primary bullet class stub to version control

---
+++ core/src/com/shootan/stellarpatrol/gameobjects/bullets/BulletPrimary.java
@@ -0,0 +1,14 @@
+package com.shootan.stellarpatrol.gameobjects.bullets;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by geeksploit on 18.09.2017.
+ */
+
+public final class BulletPrimary extends Bullet {
+
+    public BulletPrimary(Vector2 position, float angle) {
+        super(position, angle);
+    }
+}
