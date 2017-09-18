598403197787022d9750782165855d6918b7afef
geeksploit <geeksploit@shootan.com>
2017-09-18 15:25:13 +0300

    Add player class stub to version control

---
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Player.java
@@ -0,0 +1,14 @@
+package com.shootan.stellarpatrol.gameobjects.ships;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by geeksploit on 18.09.2017.
+ */
+
+public final class Player extends Ship {
+
+    public Player(Vector2 position) {
+        super(position);
+    }
+}
