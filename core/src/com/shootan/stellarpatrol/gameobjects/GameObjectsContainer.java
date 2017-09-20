698ff612709d8e65766ff9a0230d29f432312cc9
geeksploit <geeksploit@shootan.com>
2017-09-20 14:16:53 +0300

    Declare a field to store player reference

--- core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
@@ -2,6 +2,7 @@ package com.shootan.stellarpatrol.gameobjects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
+import com.shootan.stellarpatrol.gameobjects.ships.Player;
 
 /**
  * Created by geeksploit on 18.09.2017.
@@ -10,6 +11,7 @@ import com.badlogic.gdx.utils.DelayedRemovalArray;
 public final class GameObjectsContainer {
 
     private DelayedRemovalArray<GameObject> gameObjects;
+    Player player;
 
     public GameObjectsContainer() {
         gameObjects = new DelayedRemovalArray<GameObject>();
