7170c2ec711042bc2879194bdd94791e92075a2d
geeksploit <geeksploit@shootan.com>
2017-09-20 14:19:48 +0300

    Initialize player and store it among other game objects

--- core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/GameObjectsContainer.java
@@ -1,6 +1,7 @@
 package com.shootan.stellarpatrol.gameobjects;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.DelayedRemovalArray;
 import com.shootan.stellarpatrol.gameobjects.ships.Player;
 
@@ -15,6 +16,8 @@ public final class GameObjectsContainer {
 
     public GameObjectsContainer() {
         gameObjects = new DelayedRemovalArray<GameObject>();
+        this.player = new Player(new Vector2());
+        gameObjects.add(player);
     }
 
     public void update (float deltaTime) {
