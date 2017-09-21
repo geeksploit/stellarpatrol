e4ee69c94a14bf08e44fa73b7af20fae7a97c7f4
geeksploit <geeksploit@shootan.com>
2017-09-21 12:24:57 +0300

    Add enemy ship class stub to version control

---
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/Enemy.java
@@ -0,0 +1,14 @@
+package com.shootan.stellarpatrol.gameobjects.ships;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by geeksploit on 21.09.2017.
+ */
+
+public class Enemy extends Ship {
+
+    public Enemy(Vector2 position) {
+        super(position);
+    }
+}
