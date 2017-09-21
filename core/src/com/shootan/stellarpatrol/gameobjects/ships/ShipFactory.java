71c21e5441e9a772902c5093faa0ba8da0a82f44
geeksploit <geeksploit@shootan.com>
2017-09-21 13:22:41 +0300

    Make a ship to prove the concept

--- core/src/com/shootan/stellarpatrol/gameobjects/ships/ShipFactory.java
+++ core/src/com/shootan/stellarpatrol/gameobjects/ships/ShipFactory.java
@@ -1,7 +1,9 @@
 package com.shootan.stellarpatrol.gameobjects.ships;
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
+import com.shootan.stellarpatrol.util.Constants;
 
 /**
  * Created by geeksploit on 20.09.2017.
@@ -26,6 +28,18 @@ public final class ShipFactory {
     public Array<Ship> makeShip() {
         ships.clear();
         float random = MathUtils.random();
+
+        if (random < .05f) {
+            Enemy ship;
+            ship = new Enemy(new Vector2(
+                    MathUtils.random(0, Constants.WORLD_SIZE),
+                    Constants.WORLD_SIZE));
+            ship.getDestination().set(
+                    MathUtils.random(0, Constants.WORLD_SIZE),
+                    MathUtils.random(0, Constants.WORLD_SIZE));
+            ship.setTextureRegion(Constants.SHIP_TEXTURE_REGIONS.get(1));
+            ships.add(ship);
+        }
 
         return ships;
     }
