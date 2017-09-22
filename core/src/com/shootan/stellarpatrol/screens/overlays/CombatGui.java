2a58ff25cd912c9d3c7aeb07d438f7175ede9c63
geeksploit <geeksploit@shootan.com>
2017-09-22 12:27:47 +0300

    Implement font setter

--- core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
+++ core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
@@ -1,6 +1,7 @@
 package com.shootan.stellarpatrol.screens.overlays;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.shootan.stellarpatrol.StellarPatrolGame;
 
 /**
@@ -12,8 +13,14 @@ public class CombatGui {
     private StellarPatrolGame game;
     private StringBuilder stringBuilder;
 
+    private BitmapFont bitmapFont;
+
     public CombatGui() {
         game = (StellarPatrolGame) Gdx.app.getApplicationListener();
         stringBuilder = new StringBuilder();
+    }
+
+    public void setFont(BitmapFont font) {
+        this.bitmapFont = font;
     }
 }
