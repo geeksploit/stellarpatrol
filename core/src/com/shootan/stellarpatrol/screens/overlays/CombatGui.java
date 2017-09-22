8e424a09a02d1d21915eea0df40a43a9c6a57d36
geeksploit <geeksploit@shootan.com>
2017-09-22 12:33:58 +0300

    Set sprite batch projection matrix

--- core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
+++ core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
@@ -26,6 +26,7 @@ public class CombatGui {
 
     public void render(float delta) {
         screenViewport.apply();
+        spriteBatch.setProjectionMatrix(screenViewport.getCamera().combined);
     }
 
     public void setFont(BitmapFont font) {
