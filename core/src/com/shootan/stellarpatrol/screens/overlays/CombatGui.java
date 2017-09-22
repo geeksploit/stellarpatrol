1ba76b7ce03c83a99796d14e8d66bbb72bf14461
geeksploit <geeksploit@shootan.com>
2017-09-22 12:16:50 +0300

    Declare and initialize a field to store game reference

--- core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
+++ core/src/com/shootan/stellarpatrol/screens/overlays/CombatGui.java
@@ -1,11 +1,17 @@
 package com.shootan.stellarpatrol.screens.overlays;
 
+import com.badlogic.gdx.Gdx;
+import com.shootan.stellarpatrol.StellarPatrolGame;
+
 /**
  * Created by geeksploit on 22.09.2017.
  */
 
 public class CombatGui {
 
+    private StellarPatrolGame game;
+
     public CombatGui() {
+        game = (StellarPatrolGame) Gdx.app.getApplicationListener();
     }
 }
